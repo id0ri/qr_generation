c4ad3ad4ab6a5231caad4d67deac6105dd4cd805
id0ri <109003783+id0ri@users.noreply.github.com>
2022-07-14 13:56:39 +0300

    added small size

--- qr_generation.py
+++ qr_generation.py
@@ -1,26 +1,28 @@
 import qrcode
+import json
+from block_generation import Blocks
 from utilities import *
-
-
-BOX_SIZE = 8
-BORDER = 0
-COLOR_BACKGROUND = (255, 255, 255)
-QR_BACKGROUND = (255, 255, 255)
-SIZE_A4 = (1824, 1240)
 
 
 def create_qr_code(type_qr, color, input_file_link, n, m):
     with open(input_file_link, 'r') as json_file:
         link_file = json.load(json_file)
 
+    with open('library.json', 'r') as library:
+        library = json.load(library)
+
     size = correlate_size(type_qr)
     color = correlate_color(color)
-    step_x = LIBRARY[str(size)]['block_w']
-    step_y = LIBRARY[str(size)]['block_h']
 
+    step_x = library[str(size)]['block_w']
+    step_y = library[str(size)]['block_h']
+    box_size = library[str(size)]['box_size']
+    text_size = library[str(size)]['text_size']
     dept = link_file['dept']
 
-    image_background = create_background_by_size(size, n, m)
+    block_type = Blocks(step_x, step_y, box_size, text_size)
+
+    image_background = create_background_by_size(step_x, step_y, n, m)
 
     position_link = 0
     inserting_qr_x = 0
@@ -31,7 +33,7 @@ def create_qr_code(type_qr, color, input_file_link, n, m):
             qr = qrcode.QRCode(
                 version=size,
                 error_correction=qrcode.constants.ERROR_CORRECT_Q,
-                box_size=BOX_SIZE,
+                box_size=box_size,
                 border=0,
             )
             qr.add_data(link_file['data'][position_link]['link'])
@@ -39,7 +41,7 @@ def create_qr_code(type_qr, color, input_file_link, n, m):
             position_link = position_link + 1
 
             image_qr = qr.make_image(fill_color=color, back_color=QR_BACKGROUND)
-            block = create_standart_block(size, dept, text, image_qr)
+            block = block_type.class_selection(size, dept, text, image_qr)
             image_background.paste(block, (inserting_qr_x, inserting_qr_y))
 
             inserting_qr_x += step_x
